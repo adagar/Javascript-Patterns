a8bb202ebf6b95edf5b32efaefc480da2e27ce1f
Nick Garza <ngarza@cainc.com>
2019-07-16 14:20:22 -0400

    Factory pattern

--- Base/index.js
+++ Base/index.js
@@ -1,26 +1,25 @@
-let instance = null;
-
 class Car {
     constructor(doors, engine, color){
-        if(!instance){
             this.doors = doors;
             this.engine = engine;
             this.color = color;
-            instance = this;
-        } else{
-            return instance;
+    }
+}
+
+class carFactory {
+    createCar(type){
+        switch(type){
+            case 'civic':
+                return new Car(4, 'V6', 'grey');
+            case 'fiesta':
+                return new Car(2, "v4", 'red');
         }
     }
 }
 
-// class SUV extends Car{
-//     constructor(doors, engine, color){
-//         super(doors, engine, color);
-//         this.wheels = 4;
-//     }
-// }
-const civic = new Car(4, 'V6', 'grey');
-const fiesta = new Car(2, "v4", "orange");
+const factory = new carFactory();
+const civic = factory.createCar('civic');
+const fiesta = factory.createCar('fiesta');
 
 console.log(civic);
 console.log(fiesta);
