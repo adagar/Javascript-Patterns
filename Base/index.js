c9a576d8695279b8269427958e2556160ee935bf
Nick Garza <ngarza@cainc.com>
2019-07-16 10:31:33 -0400

    Constructor pattern

--- Base/index.js
+++ Base/index.js
@@ -1,1 +1,19 @@
-console.log("Hello, world!");
+class Car {
+    constructor(doors, engine, color){
+        this.doors = doors;
+        this.engine = engine;
+        this.color = color;
+    }
+}
+
+class SUV extends Car{
+    constructor(doors, engine, color){
+        super(doors, engine, color);
+        this.wheels = 4;
+    }
+}
+const civic = new Car(4, 'V6', 'grey');
+const humvee = new SUV(4, 'v8', 'blue');
+
+console.log(civic);
+console.log(humvee);
