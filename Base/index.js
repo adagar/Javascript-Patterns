798955522afcd0b1d22cba81a20730b623f35cab
Nick Garza <ngarza@cainc.com>
2019-07-16 10:41:45 -0400

    Singleton pattern

--- Base/index.js
+++ Base/index.js
@@ -1,19 +1,26 @@
+let instance = null;
+
 class Car {
     constructor(doors, engine, color){
-        this.doors = doors;
-        this.engine = engine;
-        this.color = color;
+        if(!instance){
+            this.doors = doors;
+            this.engine = engine;
+            this.color = color;
+            instance = this;
+        } else{
+            return instance;
+        }
     }
 }
 
-class SUV extends Car{
-    constructor(doors, engine, color){
-        super(doors, engine, color);
-        this.wheels = 4;
-    }
-}
+// class SUV extends Car{
+//     constructor(doors, engine, color){
+//         super(doors, engine, color);
+//         this.wheels = 4;
+//     }
+// }
 const civic = new Car(4, 'V6', 'grey');
-const humvee = new SUV(4, 'v8', 'blue');
+const fiesta = new Car(2, "v4", "orange");
 
 console.log(civic);
-console.log(humvee);
+console.log(fiesta);
